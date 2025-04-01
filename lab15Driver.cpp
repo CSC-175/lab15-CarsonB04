0974666865a56b22e9f7a278b780a924e7619c43
your name <your email>
2025-03-31 22:35:28 -0400

    1st commit

--- lab15Driver.cpp
+++ lab15Driver.cpp
@@ -1,28 +1,34 @@
-// This program finds the probability of winning a "mini" lottery
-// when the user's set of numbers match the set drawn
-// by the lottery organizers. It uses three functions.
-# include <iostream>
-# include "./lab15.cpp"
+#include <iostream>
+#include "lab15.cpp"  // Include the lab15.cpp file for functions
+
 using namespace std;
 
-int main()
-{
-    int pickFrom, // The number of numbers to pick from
-    numPicks;// The number of numbers to select
-    double ways; // The number of different possible
-                 // ways to pick the set of numbers
-    char again;
+int main() {
+    int n, k;
+    char choice;
 
-    do{
-        getInfo(pickFrom, numPicks);
-        ways = computeWays(pickFrom, numPicks);
-        cout.setf(ios::fixed);
-        cout.precision(4);
-        cout << "\nProbability of winning is " << ways;
-        cout << "\nOdds of winning are 1 in " << int(1/ways);
-        cout << "\n\nWould you like to calculate the probability of another scenario? (y/n): ";
-        cin >> again;
-    }while(again =='y' or again == 'Y');
+    do {
+        // Get lottery information from the user
+        getInfo(n, k);
+
+        // Compute the number of possible ways
+        double ways = computeWays(n, k);
+
+        // Compute the probability of winning
+        double probability = 1.0 / ways;
+
+        // Compute the odds of winning
+        double odds = ways - 1;
+
+        // Output the results
+        cout << "Probability of winning is " << probability << endl;
+        cout << "Odds of winning are 1 in " << odds << endl;
+
+        // Ask if the user wants to calculate another scenario
+        cout << "Would you like to calculate the probability of another scenario? (y/n): ";
+        cin >> choice;
+
+    } while(choice == 'y' || choice == 'Y');
+
     return 0;
 }
-
